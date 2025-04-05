056a25ddfd806dc2efed388d7de139ce5e8a1539
VishiATChoudhary <vishisht.choudhary@rwth-aachen.de>
2025-04-06 00:18:57 +0200

    deleted shit ressources

--- test_bedrock.py
+++
@@ -1,9 +0,0 @@
-from langchain_aws import ChatBedrock
-
-llm = ChatBedrock(
-    model_id="anthropic.claude-3-sonnet-20240229-v1:0",
-    model_kwargs={"temperature": 0, "max_tokens": None},
-)
-
-response = llm.invoke("Where is daddy?")
-print(response.content)
